f9e19205342319c217057615a54152767cdc2800
Foxy <tomek.cieslinski@gmail.com>
2023-05-12 21:01:31 +0200

    Update serverListen.ts

--- EXPRESS/source/serverListen.ts
+++ EXPRESS/source/serverListen.ts
@@ -24,22 +24,22 @@ router.use(express.json());
 /** RULES OF OUR API */
 router.use(cors())
 router.options('*', cors())
-router.use((req, res, next) => {
-  // set the CORS policy
-  res.header("Access-Control-Allow-Origin", req.header("origin"));
-  res.header("Access-Control-Allow-Origin", "*");
-  // set the CORS headers
-  res.header(
-    "Access-Control-Allow-Headers",
-    "origin, X-Requested-With,Content-Type,Accept, Authorization"
-  );
-  // set the CORS method headers
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
-    return res.status(200).json({});
-  }
-  next();
-});
+// router.use((req, res, next) => {
+//   // set the CORS policy
+//   res.header("Access-Control-Allow-Origin", req.header("origin"));
+//   res.header("Access-Control-Allow-Origin", "*");
+//   // set the CORS headers
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "origin, X-Requested-With,Content-Type,Accept, Authorization"
+//   );
+//   // set the CORS method headers
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
 /** Routes */
 router.use("/", routes);
