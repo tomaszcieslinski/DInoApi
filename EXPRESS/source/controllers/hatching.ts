e9b3cb8cea2f8d342085ef0eba48f8eb01fc8f8a
Foxy <tomek.cieslinski@gmail.com>
2023-05-17 21:22:59 +0200

    Update hatching.ts

--- EXPRESS/source/controllers/hatching.ts
+++ EXPRESS/source/controllers/hatching.ts
@@ -48,9 +48,8 @@ const getoNftTraitsByAttr = async (req: Request, response: Response)=>{
 const postoNftFilter = async(req: Request,response:Response)=>{
   let data = req.body;
   let res =  await nftservice.returnFilteredo(data)
-  return response.status(200).json(res)
+  return response.status(200).json(res.sort(() => Math.random() - 0.5))
 }
-
 
 const getNftTraits = async (req: Request, response: Response)=>{
   let res = await nftservice.getTraits()
@@ -65,7 +64,7 @@ const getNftTraitsByAttr = async (req: Request, response: Response)=>{
 const postNftFilter = async(req: Request,response:Response)=>{
   let data = req.body;
   let res =  await nftservice.returnFiltered(data)
-  return response.status(200).json(res)
+  return response.status(200).json(res.sort(() => Math.random() - 0.5))
 }
 
 
