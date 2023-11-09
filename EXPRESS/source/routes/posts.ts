eed56460eb7ca5487c097c29ae74015acb5bb50a
foxy98pl <58765640+foxy98PL@users.noreply.github.com>
2023-11-09 18:54:14 +0100

    test

--- EXPRESS/source/routes/posts.ts
+++ EXPRESS/source/routes/posts.ts
@@ -1,5 +1,5 @@
 /** source/routes/posts.ts */
-import express from "express";
+import express, {Request, Response, NextFunction } from 'express';
 import controller from "../controllers/transactions";
 import nftholders from "../controllers/hatching"
 import burn from "../controllers/burn";
@@ -7,6 +7,14 @@ import staking from "../controllers/staking";
 import nftservice from "../services/nftservice";
 const router = express.Router();
 
+declare global {
+    namespace Express {
+      interface Request {
+        user: any;
+      }
+    }
+  }
+  
 //TransactionRoutes
 router.get("/transactions", controller.getTransactions);
 router.get("/transactions/walletRank", controller.getWalletRank);
@@ -38,4 +46,49 @@ router.get("/burn/list",burn.getBurnedByWallet)
 router.get("/staking",staking.getStakingRanking)
 router.get("/staking/walletRank",staking.getStakingWalletRank)
 router.get("/staking/list",staking.getStakedByWallet)
-export = router;
+
+
+import jwt from 'jsonwebtoken';
+
+const SECRET_KEY = process.env.SECRET_KEY || 'DinoBetSecret20239988';
+
+function authenticateToken(req: Request, res: Response, next: NextFunction) {
+  const token = req.header('Authorization');
+
+  if (token === undefined) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  jwt.verify(token.replace('Bearer ', ''), SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Forbidden' });
+    }
+    req.user = user;
+  });
+}
+
+router.post('dinobetapi/gameData', authenticateToken, (req: Request, res: Response) => {
+  res.json({ message: 'Protected route accessed successfully' });
+});
+
+router.post('dinobetapi/login', (req: Request, res: Response) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const expiresIn = 3650 * 24 * 60 * 60; // 10 years in seconds
+
+  if (username == "dinobet" && password == "DinoBetSecret20239988") {
+    const user = { name: username };
+    const token = jwt.sign(user, SECRET_KEY, { expiresIn });
+    res.json({ token });
+  } else {
+    res.status(401).json({ message: "Check Username or password" });
+  }
+});
+
+
+
+
+
+
+
+export default router;
